903468fa58833a016bfb2ed2907082a2b384743b
Atharva Upadhye <70993547+atharva-upadhye@users.noreply.github.com>
2024-11-19 20:53:27 +0530

    test commit 3

--- src/diff.test.ts
+++ src/diff.test.ts
@@ -1,10 +1,10 @@
 import { expect, test } from "vitest";
 import { diff } from "./diff";
 
-test("adds 6 - 3 to equal 3", () => {
+test("6 - 3 to equal 3", () => {
 	expect(diff(6, 3)).toBe(3);
 });
 
-test("adds 1 - 8 to equal -7", () => {
+test("1 - 8 to equal -7", () => {
 	expect(diff(1, 8)).toBe(-7);
 });
